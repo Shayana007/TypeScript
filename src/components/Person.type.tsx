f8bc879a4dc6ac54be838ba92597fae246b89348
Shayana007 <shayanagowda1234@gmail.com>
2022-05-23 12:35:11 +0530

    adding typescript files

---
+++ src/components/Person.type.tsx
@@ -0,0 +1,2 @@
+export type Name = { first: string; last: string };
+export type PersonProps = { name: Name };
---
+++ src/components/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  primary: {
+    main: "#3f51b5",
+
+    text: "#fff",
+  },
+
+  secondary: {
+    main: "#5f0057",
+
+    text: "#fff",
+  },
+};
